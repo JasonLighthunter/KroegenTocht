b9b0b2ff4ddb694a29c65522a4945aa746513838
stev43 <steven.v.d.vorm@hotmail.com>
2017-03-16 15:49:49 +0100

    Place.js > string identifiers made non string

--- app/models/place.js
+++ app/models/place.js
@@ -3,37 +3,37 @@ var mongoose = require('mongoose');
 console.log('Initializing places schema');
 
 var placeSchema = new mongoose.Schema({
-      "geometry" : {
-        "location" : {
-          "lat" : Number,
-          "lng" : Number
+      geometry : {
+        location : {
+          lat : Number,
+          lng : Number
         }
       },
-      "icon" : String,
-      "id" : String,
-      "name" : String,
-      "opening_hours" : {
-        "open_now" : Boolean
+      icon : String,
+      id : String,
+      name : String,
+      opening_hours : {
+        open_now : Boolean
       },
-      "photos" : [
+      photos : [
         {
-          "height" : Number,
-          "html_attributions" : [],
-          "photo_reference" : String,
-          "width" : Number
+          height : Number,
+          html_attributions : [],
+          photo_reference : String,
+          width : Number
         }
       ],
-      "place_id" : String,
-      "scope" : String,
-      "alt_ids" : [
+      place_id : String,
+      scope : String,
+      alt_ids : [
         {
-          "place_id" : String,
-          "scope" : String
+          place_id : String,
+          scope : String
         }
       ],
-      "reference" : String,
-      "types" : [ String ],
-      "vicinity" : String
+      reference : String,
+      types : [ String ],
+      vicinity : String
 });
 
 placeSchema.query.byPlaceId = function(place_id) {
