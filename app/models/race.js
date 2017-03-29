a4788441327e5d805de1e0781c2198496be30d49
stev43 <steven.v.d.vorm@hotmail.com>
2017-03-29 12:24:34 +0200

    added query strings / URL filter

--- app/models/race.js
+++ app/models/race.js
@@ -4,10 +4,14 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var raceSchema = mongoose.Schema({
-  testString : String,
+  name : String,
   boolean    : Boolean,
   age        : Number
 });
+
+raceSchema.query.byName = function(nameQuery) {
+  return this.where('name').equals(nameQuery);
+}
 
 // create the model for races and expose it to our app
 module.exports = mongoose.model('Race', raceSchema);
