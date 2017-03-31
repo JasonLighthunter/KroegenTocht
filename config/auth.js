c9c0aacc58bec715f3c3f9e491ca7aafed66f687
Job Cuppen <jobcuppen@live.nl>
2017-03-31 09:29:47 +0200

    localhost to remote host

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
   'googleAuth' : {
     'clientID'     : '781738022894-efon6bv0h7877026rlsb6lofrfef8sjk.apps.googleusercontent.com',
     'clientSecret' : 'gsN4mrOaeCGKC0TSeHsbhkzI',
-    'callbackURL'  : 'http://127.0.0.1:8080/auth/google/callback'
+    'callbackURL'  : process.env.GOOGLE_CALLBACK || 'http://127.0.0.1:8080/auth/google/callback'
   },
   'redditAuth' : {
     'clientID'      : 'BwnKz59fD292Qw',
     'clientSecret'  : 'q_NHWt0HV0zY76pRL5EHSOKT5N4',
-    'callbackURL'   : 'http://127.0.0.1:8080/auth/reddit/callback'  
+    'callbackURL'   : process.env.REDDIT_CALLBACK || 'http://127.0.0.1:8080/auth/reddit/callback'  
   }
 };
